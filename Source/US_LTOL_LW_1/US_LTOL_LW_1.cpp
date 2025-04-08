d7e6bb999e2d8522bb2d2f7bb348e6988dcabbb0
LenaFWU <lena.f.wu@gmail.com>
2025-04-08 11:08:38 -0400

    Update ignore

--- Source/US_LTOL_LW_1/US_LTOL_LW_1.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "US_LTOL_LW_1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, US_LTOL_LW_1, "US_LTOL_LW_1" );
