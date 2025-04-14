77ddd06f9a826ceec99ef0b45eb32c37dd5cd8b4
LenaFWU <lena.f.wu@gmail.com>
2025-04-13 23:36:30 -0400

    Add character health in BP_Character

--- Source/US_LTOL_LW_1/US_PlayerState.cpp
+++ Source/US_LTOL_LW_1/US_PlayerState.cpp
@@ -4,12 +4,15 @@
 #include "US_Character.h"
 #include "US_CharacterStats.h"
 #include "Net/UnrealNetwork.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/GameModeBase.h"
 
 void AUS_PlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty> &OutLifetimeProps) const
 {
     Super::GetLifetimeReplicatedProps(OutLifetimeProps);
     DOREPLIFETIME_CONDITION(AUS_PlayerState, Xp, COND_OwnerOnly);
     DOREPLIFETIME_CONDITION(AUS_PlayerState, CharacterLevel, COND_OwnerOnly);
+    DOREPLIFETIME_CONDITION(AUS_PlayerState, Health, COND_OwnerOnly);
 }
 
 void AUS_PlayerState::AddXp(const int32 Value)
@@ -37,4 +40,38 @@ void AUS_PlayerState::OnRep_Xp(int32 OldValue) const
 void AUS_PlayerState::OnRep_CharacterLevelUp(int32 OldValue) const
 {
     OnCharacterLevelUp.Broadcast(CharacterLevel);
+}
+
+void AUS_PlayerState::ApplyDamage(float Amount)
+{
+    if (GetLocalRole() != ROLE_Authority)
+        return;
+
+    Health -= Amount;
+
+    if (Health <= 0)
+    {
+        Health = 0;
+
+        FTimerDelegate RespawnDelegate;
+        RespawnDelegate.BindWeakLambda(this, [this]()
+                                       {
+            if (AGameModeBase* GM = Cast<AGameModeBase>(UGameplayStatics::GetGameMode(GetWorld())))
+            {
+                if (AController* Controller = Cast<AController>(GetOwner()))
+                {
+                    GM->RestartPlayer(Controller);
+                }
+            } });
+
+        FTimerHandle RespawnTimerHandle;
+        GetWorld()->GetTimerManager().SetTimer(RespawnTimerHandle, RespawnDelegate, 2.f, false);
+    }
+
+    OnHealthChanged.Broadcast(Health);
+}
+
+void AUS_PlayerState::OnRep_Health()
+{
+    OnHealthChanged.Broadcast(Health);
 }
--- Source/US_LTOL_LW_1/US_PlayerState.h
+++ Source/US_LTOL_LW_1/US_PlayerState.h
@@ -12,6 +12,7 @@
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnXpChanged, int32, NewXp);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnCharacterLevelUp, int32, NewLevelXp);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChanged, float, NewHealth);
 
 UCLASS()
 class US_LTOL_LW_1_API AUS_PlayerState : public APlayerState
@@ -19,23 +20,39 @@ class US_LTOL_LW_1_API AUS_PlayerState : public APlayerState
 	GENERATED_BODY()
 
 protected:
+	// xp and level
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, ReplicatedUsing = "OnRep_Xp", Category = "Experience")
 	int Xp = 0;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, ReplicatedUsing = "OnRep_CharacterLevelUp", Category = "Experience")
 	int CharacterLevel = 1;
+
+	// health
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, ReplicatedUsing = "OnRep_Health", Category = "Health")
+	float Health = 100.0f;
+
 	UFUNCTION()
 	void OnRep_Xp(int32 OldValue) const;
 	UFUNCTION()
 	void OnRep_CharacterLevelUp(int32 OldValue) const;
+	UFUNCTION()
+	void OnRep_Health();
 
 	UPROPERTY(BlueprintAssignable, Category = "Events")
 	FOnXpChanged OnXpChanged;
 	UPROPERTY(BlueprintAssignable, Category = "Events")
 	FOnCharacterLevelUp OnCharacterLevelUp;
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnHealthChanged OnHealthChanged;
 
 public:
 	UFUNCTION(BlueprintCallable, Category = "Experience")
 	void AddXp(int32 Value);
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	void ApplyDamage(float Amount);
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	float GetHealth() const { return Health; }
 
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty> &OutLifetimeProps) const override;
 };
